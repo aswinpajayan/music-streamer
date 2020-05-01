f355bd92b7eb43d1d0ff5ac1072741a091363267
Aswin P Ajayan <aswinp2008@gmail.com>
2020-05-01 15:02:47 +0530

    removed hard coded values

--- get_music.py
+++ get_music.py
@@ -1,39 +1,53 @@
-#!/usr/bin/python3
-import youtube_dl
+'''get the youtube links for music videos'''
 
+import json
+import random
 
 class MusicBrowser:
     ''' Class to get links for youtube videos
     args : artist name'''
-    def __init__(self, artist_name):
-        self.artist = "site:youtube.com (intitle:hits OR song OR mashup)\"" + artist_name + "\" -channel"
+    def __init__(self, artist, params):
+        self.num = params['num_of_tracks']
+        self.query = "site:" + params['site']
+        self.query += " (intitle:" + " OR ".join(params['intitle']) + ")"
+        self.query += " \"" + artist['name'] + "\" -channel"
+        if 'tags' in artist:
+            self.query += " " + " ".join(artist['tags'])
         self.tracks = ""
-
+        print(self.query)
     def get_tracks(self):
         ''' method to generate links for youtube videos '''
         try:
             from googlesearch import search
         except ImportError:
             print("No module named 'google' Found, check venv")
-        self.tracks = search(query=self.artist,
-                             lang='en', num=10, stop=5)
+        self.tracks = search(query=self.query,
+                             lang='en', num=self.num, stop=self.num, pause=20)
 
     def get_playlists(self, get_list=False):
         ''' is_list = True returns playlist url'''
         if get_list:
-            self.artist = self.artist + " AND playlist"
+            self.query = self.query + " AND playlist"
         else:
-            self.artist = self.artist + " -playlist"
+            self.query = self.query + " -playlist"
 
 
 def main():
     '''main method'''
     playlist = []
-    sithara = MusicBrowser("Sithara Krishnakumar")
-    sithara.get_tracks()
-    for track in sithara.tracks:
-        playlist.append(track)
+    with open("conf.json", "r") as global_conf:
+        conf = (json.load(global_conf))
+    for artist in conf['artists']:
+        print(artist['name'])
+        browser = MusicBrowser(artist, params=conf['global_params'])
+        browser.get_tracks()
+        for track in browser.tracks:
+            playlist.append(track)
+    random.shuffle(playlist)
     print(playlist)
+    playlist = "\n".join(playlist)
+    with open("links.tmp", "w") as link_file:
+        link_file.write(playlist)
 
 
 if __name__ == '__main__':
